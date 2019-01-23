d73b16c8075aa6f79c84f6eb3019999b558f7d9e
mmacranderuno <mmacrander@unomaha.edu>
2019-01-22 22:10:09 -0600

    Nuked it all.  Fiddling with hosts.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v2$4%_w=bf1uld@lgmm&3pkjv7i#o21haf5r_%=mi1j$+qinjj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
